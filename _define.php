7fbec05fbb0e58eb05535e38a71bf25df4a09adc
franck <carnet.franck.paul@gmail.com>
2023-06-20 10:36:27 +0200

    Don't use admin URLs in help files

--- _define.php
+++ _define.php
@@ -14,7 +14,7 @@ $this->registerModule(
     'PrivateMode',
     'Protect your blog with a password',
     'Osku and contributors',
-    '3.1',
+    '3.2',
     [
         'requires'    => [['core', '2.26']],
         'permissions' => dcCore::app()->auth->makePermissions([
