29b8cc3ca2448b549721cb467937eaa1c7d360e1
franck <carnet.franck.paul@gmail.com>
2025-09-12 18:41:48 +0200

    New release 7.5

--- _define.php
+++ _define.php
@@ -15,9 +15,9 @@ $this->registerModule(
     'PrivateMode',
     'Protect your blog with a password',
     'Osku and contributors',
-    '7.4',
+    '7.5',
     [
-        'date'     => '2025-09-07T15:50:54+0200',
+        'date'     => '2025-09-12T18:41:48+0200',
         'requires' => [
             ['core', '2.36'],
             ['TemplateHelper'],
