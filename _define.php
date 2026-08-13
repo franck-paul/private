3d1cb04222ad449e5c72c11b956fb7013e2a1aac
franck <contact@open-time.net>
2026-08-13 16:44:54 +0200

    Enforces some verification before registering module

--- _define.php
+++ _define.php
@@ -11,24 +11,28 @@
  * @copyright Osku
  * @copyright GPL-2.0 https://www.gnu.org/licenses/gpl-2.0.html
  */
-$this->registerModule(
-    'PrivateMode',
-    'Protect your blog with a password',
-    'Osku and contributors',
-    '8.0',
-    [
-        'date'     => '2026-08-03T10:09:02+0200',
-        'requires' => [
-            ['core', '2.39'],
-            ['TemplateHelper'],
-        ],
-        'permissions' => 'My',
-        'priority'    => 1501,
-        'type'        => 'plugin',
+declare(strict_types=1);
 
-        'details'    => 'https://open-time.net/?q=private',
-        'support'    => 'https://github.com/franck-paul/private',
-        'repository' => 'https://raw.githubusercontent.com/franck-paul/private/main/dcstore.xml',
-        'license'    => 'gpl2',
-    ]
-);
+if (isset($this) && is_object($this) && method_exists($this, 'registerModule') && isset($this->id) && is_string($this->id)) {
+    $this->registerModule(
+        'PrivateMode',
+        'Protect your blog with a password',
+        'Osku and contributors',
+        '8.0',
+        [
+            'date'     => '2026-08-03T10:09:02+0200',
+            'requires' => [
+                ['core', '2.39'],
+                ['TemplateHelper'],
+            ],
+            'permissions' => 'My',
+            'priority'    => 1501,
+            'type'        => 'plugin',
+
+            'details'    => 'https://open-time.net/?q=private',
+            'support'    => 'https://github.com/franck-paul/private',
+            'repository' => 'https://raw.githubusercontent.com/franck-paul/private/main/dcstore.xml',
+            'license'    => 'gpl2',
+        ]
+    );
+}
